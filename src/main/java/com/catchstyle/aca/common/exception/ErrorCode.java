80e0e3ae33218f3faebfa18ea658e245fbc0a0e7
nomellc <jyj100743@ewha.ac.kr>
2026-07-15 23:49:43 +0900

    feat: 커스텀 예외 처리 작성

---
+++ src/main/java/com/catchstyle/aca/common/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.catchstyle.aca.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "게시글을 찾을 수 없습니다."),
+    INVALID_POST_ID(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 게시글 ID입니다.");
+
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
+}
